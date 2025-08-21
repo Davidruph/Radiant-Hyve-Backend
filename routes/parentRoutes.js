653692aa9b3ba4f2b1714dc3827afdb7d61ac550
Mitul <parthr.cubes@gmail.com>
2025-08-21 11:30:45 +0530

    Update parentRoutes to rename endpoints for listing student invoices and fees for clarity and consistency.

--- routes/parentRoutes.js
+++ routes/parentRoutes.js
@@ -26,8 +26,8 @@ router.put('/edit_profile_parent',verifyToken, imageUpload, validation.editparen
 
 router.get('/list_student_attedance_parent',verifyToken, parent.getStudentAttedance);
 
-router.get('/list_student_invoice',verifyToken, student.listStudentFees);
-router.get('/list_student_fees',verifyToken, student.listStudent);
+router.get('/list_students_invoice',verifyToken, student.listStudentFees);
+router.get('/list_students_fees',verifyToken, student.listStudent);
 
 
 module.exports = router; 
