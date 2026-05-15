fbfd7a6a0379e02e620bdb4b03c1f1672c4efab1
Agbugba David Chinweuba <57929456+Davidruph@users.noreply.github.com>
2026-05-15 14:44:19 +0100

    push changes

---
+++ config/db.js
@@ -0,0 +1,461 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = require("./connection");
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.User = require("../model/user")(sequelize, Sequelize, Model);
+db.Token = require("../model/token")(sequelize, Sequelize, Model);
+db.Student = require("../model/student")(sequelize, Sequelize, Model);
+db.Shift = require("../model/shift")(sequelize, Sequelize, Model);
+db.Menu = require("../model/menu")(sequelize, Sequelize, Model);
+
+db.StudentMenu = require("../model/studentMenu")(sequelize, Sequelize, Model);
+db.StudentAttendance = require("../model/studentAttendance")(
+  sequelize,
+  Sequelize,
+  Model
+);
+db.SleepLoag = require("../model/sleepLoag")(sequelize, Sequelize, Model);
+db.Chat = require("../model/chat")(sequelize, Sequelize, Model);
+db.Event = require("../model/event")(sequelize, Sequelize, Model);
+db.Leave = require("../model/leave")(sequelize, Sequelize, Model);
+db.Message = require("../model/messsage")(sequelize, Sequelize, Model);
+db.MessageStatus = require("../model/messageStatus")(
+  sequelize,
+  Sequelize,
+  Model
+);
+db.MenuDay = require("../model/menuDay")(sequelize, Sequelize, Model);
+db.MedicationInfo = require("../model/medicationInfo")(
+  sequelize,
+  Sequelize,
+  Model
+);
+db.Certification = require("../model/certification")(
+  sequelize,
+  Sequelize,
+  Model
+);
+db.Attendance = require("../model/attendance")(sequelize, Sequelize, Model);
+db.AddRole = require("../model/addRole")(sequelize, Sequelize, Model);
+db.Notification = require("../model/notification")(sequelize, Sequelize, Model);
+db.Sos = require("../model/sos")(sequelize, Sequelize, Model);
+db.Invoice = require("../model/invoice")(sequelize, Sequelize, Model);
+db.SosType = require("../model/sosType")(sequelize, Sequelize, Model);
+db.Menu = require("../model/menu")(sequelize, Sequelize, Model);
+db.Bath = require("../model/bath")(sequelize, Sequelize, Model);
+db.Diaper = require("../model/diaper")(sequelize, Sequelize, Model);
+db.SubscriptionPlan = require("../model/subscriptionPlan")(
+  sequelize,
+  Sequelize,
+  Model
+);
+db.Feature = require("../model/feature")(sequelize, Sequelize, Model);
+db.Subscriber = require("../model/subscriber")(sequelize, Sequelize, Model);
+db.Transaction = require("../model/transaction")(sequelize, Sequelize, Model);
+
+// Transportation Models
+db.Vehicle = require("../model/vehicle")(sequelize, DataTypes);
+db.Route = require("../model/route")(sequelize, DataTypes);
+db.RouteStop = require("../model/routeStop")(sequelize, DataTypes);
+db.StudentTransport = require("../model/studentTransport")(
+  sequelize,
+  DataTypes
+);
+db.DropOffRecipient = require("../model/dropOffRecipient")(
+  sequelize,
+  DataTypes
+);
+db.TransportLog = require("../model/transportLog")(sequelize, DataTypes);
+db.TransportException = require("../model/transportException")(
+  sequelize,
+  DataTypes
+);
+db.DriverLocation = require("../model/driverLocation")(sequelize, DataTypes);
+
+db.Student.hasMany(db.Invoice, { foreignKey: "student_id", as: "invoice" });
+db.Invoice.belongsTo(db.Student, { foreignKey: "student_id", as: "student" });
+
+db.User.hasMany(db.Notification, {
+  foreignKey: "notification_by",
+  as: "Notification"
+});
+db.Notification.belongsTo(db.User, {
+  foreignKey: "notification_by",
+  as: "notificationby"
+});
+
+db.User.hasMany(db.Notification, {
+  foreignKey: "notification_to",
+  as: "Notifications"
+});
+db.Notification.belongsTo(db.User, {
+  foreignKey: "notification_to",
+  as: "notificationto"
+});
+
+db.User.hasMany(db.AddRole, { foreignKey: "school_id", as: "School" });
+db.AddRole.belongsTo(db.User, { foreignKey: "school_id", as: "addSchool" });
+
+db.User.hasMany(db.Student, { foreignKey: "parent_id", as: "Students" });
+db.Student.belongsTo(db.User, { foreignKey: "parent_id", as: "StudentParent" });
+
+db.Shift.hasMany(db.Student, { foreignKey: "shift_id", as: "studentShift" });
+db.Student.belongsTo(db.Shift, { foreignKey: "shift_id", as: "Shift" });
+
+db.User.hasMany(db.Student, { foreignKey: "teacher_id", as: "Student" });
+db.Student.belongsTo(db.User, { foreignKey: "teacher_id", as: "Teacher" });
+
+db.User.hasMany(db.AddRole, { foreignKey: "add_to", as: "AddToRole" });
+db.AddRole.belongsTo(db.User, { foreignKey: "add_to", as: "AddRole" });
+
+db.User.hasMany(db.AddRole, { foreignKey: "add_by", as: "AddByRole" });
+db.AddRole.belongsTo(db.User, { foreignKey: "add_by", as: "AddedRole" });
+
+db.User.hasMany(db.MessageStatus, {
+  foreignKey: "message_by",
+  as: "MessageStatuse"
+});
+db.MessageStatus.belongsTo(db.User, { foreignKey: "message_by", as: "Users" });
+
+db.User.hasMany(db.MessageStatus, {
+  foreignKey: "message_to",
+  as: "MessageStatus"
+});
+db.MessageStatus.belongsTo(db.User, { foreignKey: "message_to", as: "User" });
+
+db.Chat.hasMany(db.MessageStatus, {
+  foreignKey: "chat_id",
+  as: "ChatMessageStatus"
+});
+db.MessageStatus.belongsTo(db.Chat, { foreignKey: "chat_id", as: "Chat" });
+
+db.Message.hasMany(db.MessageStatus, {
+  foreignKey: "message_id",
+  as: "MessageStatus"
+});
+db.MessageStatus.belongsTo(db.Message, {
+  foreignKey: "message_id",
+  as: "Message"
+});
+
+db.User.hasMany(db.Attendance, { foreignKey: "user_id", as: "userAttend" });
+db.Attendance.belongsTo(db.User, {
+  foreignKey: "user_id",
+  as: "userAttendance"
+});
+
+db.User.hasMany(db.Attendance, {
+  foreignKey: "school_id",
+  as: "schoolAttendance"
+});
+db.Attendance.belongsTo(db.User, {
+  foreignKey: "school_id",
+  as: "schoolAttendance"
+});
+
+db.User.hasMany(db.Certification, {
+  foreignKey: "admin_id",
+  as: "certificationAdmin"
+});
+db.Certification.belongsTo(db.User, {
+  foreignKey: "admin_id",
+  as: "staffCertificats"
+});
+
+db.User.hasMany(db.Certification, {
+  foreignKey: "school_id",
+  as: "schoolCertification"
+});
+db.Certification.belongsTo(db.User, {
+  foreignKey: "school_id",
+  as: "schoolCertificats"
+});
+
+db.User.hasMany(db.Certification, {
+  foreignKey: "staff_id",
+  as: "staffCertification"
+});
+db.Certification.belongsTo(db.User, {
+  foreignKey: "staff_id",
+  as: "certificats"
+});
+
+db.User.hasMany(db.Chat, { foreignKey: "chat_by", as: "ChatsByUser" });
+db.Chat.belongsTo(db.User, { foreignKey: "chat_by", as: "Sender" });
+
+db.User.hasMany(db.Chat, { foreignKey: "chat_to", as: "ChatsToUser" });
+db.Chat.belongsTo(db.User, { foreignKey: "chat_to", as: "Receiver" });
+
+db.User.hasMany(db.Chat, { foreignKey: "school_id", as: "schoolChat" });
+db.Chat.belongsTo(db.User, { foreignKey: "school_id", as: "ChatSchool" });
+
+db.Chat.hasMany(db.Message, { foreignKey: "chat_id", as: "Messages" });
+db.Message.belongsTo(db.Chat, { foreignKey: "chat_id", as: "ChatMessages" });
+
+db.User.hasMany(db.Message, { foreignKey: "message_by", as: "sentMessages" });
+db.Message.belongsTo(db.User, {
+  foreignKey: "message_by",
+  as: "sendermessage"
+});
+
+db.User.hasMany(db.Message, {
+  foreignKey: "message_to",
+  as: "receivedMessages"
+});
+db.Message.belongsTo(db.User, {
+  foreignKey: "message_to",
+  as: "receivermessage"
+});
+
+db.User.hasMany(db.Event, { foreignKey: "admin_id", as: "EventAdmin" });
+db.Event.belongsTo(db.User, { foreignKey: "admin_id", as: "AdminEvents" });
+
+db.User.hasMany(db.Event, { foreignKey: "school_id", as: "schoolEvent" });
+db.Event.belongsTo(db.User, { foreignKey: "school_id", as: "Events" });
+
+db.User.hasMany(db.Leave, { foreignKey: "teacher_id", as: "teachersLeave" });
+db.Leave.belongsTo(db.User, { foreignKey: "teacher_id", as: "LeaveTeacher" });
+
+db.User.hasMany(db.MedicationInfo, {
+  foreignKey: "admin_id",
+  as: "adminMedicalInfo"
+});
+db.MedicationInfo.belongsTo(db.User, {
+  foreignKey: "admin_id",
+  as: "adminMedicationInfo"
+});
+
+db.User.hasMany(db.MedicationInfo, {
+  foreignKey: "school_id",
+  as: "schoolMedicationInfo"
+});
+db.MedicationInfo.belongsTo(db.User, {
+  foreignKey: "school_id",
+  as: "Medication"
+});
+
+db.Student.hasMany(db.MedicationInfo, {
+  foreignKey: "student_id",
+  as: "studentMedicalInfo"
+});
+db.MedicationInfo.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "MedicationInfoStudent"
+});
+
+db.User.hasMany(db.Menu, { foreignKey: "admin_id", as: "adminMenu" });
+db.Menu.belongsTo(db.User, { foreignKey: "admin_id", as: "studentMenu" });
+
+db.User.hasMany(db.Menu, { foreignKey: "school_id", as: "schoolMenu" });
+db.Menu.belongsTo(db.User, { foreignKey: "school_id", as: "Menu" });
+
+db.Menu.hasMany(db.MenuDay, { foreignKey: "menu_id", as: "MenuDay" });
+db.MenuDay.belongsTo(db.Menu, { foreignKey: "menu_id", as: "dayMenu" });
+
+db.Student.hasMany(db.Menu, { foreignKey: "student_id", as: "StudentsMenu" });
+db.Menu.belongsTo(db.Student, { foreignKey: "student_id", as: "student" });
+
+db.User.hasMany(db.Shift, { foreignKey: "admin_id", as: "adminShift" });
+db.Shift.belongsTo(db.User, { foreignKey: "admin_id", as: "Shiftadmin" });
+
+db.Student.hasOne(db.SleepLoag, { foreignKey: "student_id", as: "SleepLoag" });
+db.SleepLoag.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "studentSleepLoag"
+});
+
+// db.User.hasMany(db.SleepLoag, {foreignKey: 'parent_id',as: 'studentSleepLoag'})
+// db.SleepLoag.belongsTo(db.User, {foreignKey: 'parent_id', as: 'SleepLoag'})
+
+db.Student.hasMany(db.StudentAttendance, {
+  foreignKey: "student_id",
+  as: "Attendance"
+});
+db.StudentAttendance.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "studentAttendance"
+});
+
+db.User.hasMany(db.StudentAttendance, {
+  foreignKey: "teacher_id",
+  as: "studentAttendance"
+});
+db.StudentAttendance.belongsTo(db.User, {
+  foreignKey: "teacher_id",
+  as: "Attendance"
+});
+
+db.Student.hasMany(db.StudentMenu, {
+  foreignKey: "student_id",
+  as: "StudentMenu"
+});
+db.StudentMenu.belongsTo(db.Student, { foreignKey: "student_id", as: "Menu" });
+
+db.Menu.hasMany(db.StudentMenu, { foreignKey: "menu_id", as: "StudentMenu" });
+db.StudentMenu.belongsTo(db.Menu, { foreignKey: "menu_id", as: "studentMenu" });
+
+db.User.hasMany(db.User, { foreignKey: "school_id", as: "schoolUser" });
+db.User.belongsTo(db.User, { foreignKey: "school_id", as: "User" });
+
+// Subscription Plan Associations
+db.SubscriptionPlan.hasMany(db.Feature, {
+  foreignKey: "plan_id",
+  as: "Features"
+});
+db.Feature.belongsTo(db.SubscriptionPlan, {
+  foreignKey: "plan_id",
+  as: "Plan"
+});
+
+db.SubscriptionPlan.hasMany(db.Subscriber, {
+  foreignKey: "plan_id",
+  as: "Subscribers"
+});
+db.Subscriber.belongsTo(db.SubscriptionPlan, {
+  foreignKey: "plan_id",
+  as: "Plan"
+});
+
+db.Subscriber.hasMany(db.Transaction, {
+  foreignKey: "subscription_id",
+  as: "Transactions"
+});
+db.Transaction.belongsTo(db.Subscriber, {
+  foreignKey: "subscription_id",
+  as: "Subscription"
+});
+
+db.User.hasMany(db.Subscriber, {
+  foreignKey: "user_id",
+  as: "Subscriptions"
+});
+db.Subscriber.belongsTo(db.User, {
+  foreignKey: "user_id",
+  as: "User"
+});
+
+// Transportation Associations
+db.User.hasMany(db.Vehicle, { foreignKey: "school_id", as: "vehicles" });
+db.Vehicle.belongsTo(db.User, { foreignKey: "school_id", as: "school" });
+
+db.User.hasMany(db.Vehicle, {
+  foreignKey: "driver_id",
+  as: "assignedVehicles"
+});
+db.Vehicle.belongsTo(db.User, { foreignKey: "driver_id", as: "driver" });
+
+db.User.hasMany(db.Route, { foreignKey: "school_id", as: "routes" });
+db.Route.belongsTo(db.User, { foreignKey: "school_id", as: "school" });
+
+db.Vehicle.hasMany(db.Route, { foreignKey: "vehicle_id", as: "routes" });
+db.Route.belongsTo(db.Vehicle, { foreignKey: "vehicle_id", as: "vehicle" });
+
+db.User.hasMany(db.Route, { foreignKey: "driver_id", as: "drivingRoutes" });
+db.Route.belongsTo(db.User, { foreignKey: "driver_id", as: "driver" });
+
+db.Route.hasMany(db.RouteStop, { foreignKey: "route_id", as: "stops" });
+db.RouteStop.belongsTo(db.Route, { foreignKey: "route_id", as: "route" });
+
+db.Route.hasMany(db.StudentTransport, {
+  foreignKey: "route_id",
+  as: "students"
+});
+db.StudentTransport.belongsTo(db.Route, {
+  foreignKey: "route_id",
+  as: "route"
+});
+
+db.Student.hasMany(db.StudentTransport, {
+  foreignKey: "student_id",
+  as: "transportAssignments"
+});
+db.StudentTransport.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "student"
+});
+
+db.RouteStop.hasMany(db.StudentTransport, {
+  foreignKey: "route_stop_id",
+  as: "students"
+});
+db.StudentTransport.belongsTo(db.RouteStop, {
+  foreignKey: "route_stop_id",
+  as: "stop"
+});
+
+db.Student.hasMany(db.DropOffRecipient, {
+  foreignKey: "student_id",
+  as: "recipients"
+});
+db.DropOffRecipient.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "student"
+});
+
+db.User.hasMany(db.DropOffRecipient, {
+  foreignKey: "school_id",
+  as: "dropoffRecipients"
+});
+db.DropOffRecipient.belongsTo(db.User, {
+  foreignKey: "school_id",
+  as: "school"
+});
+
+db.Route.hasMany(db.TransportLog, { foreignKey: "route_id", as: "logs" });
+db.TransportLog.belongsTo(db.Route, { foreignKey: "route_id", as: "route" });
+
+db.User.hasMany(db.TransportLog, { foreignKey: "driver_id", as: "driverLogs" });
+db.TransportLog.belongsTo(db.User, { foreignKey: "driver_id", as: "driver" });
+
+db.Student.hasMany(db.TransportLog, {
+  foreignKey: "student_id",
+  as: "transportLogs"
+});
+db.TransportLog.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "student"
+});
+
+db.Route.hasMany(db.TransportException, {
+  foreignKey: "route_id",
+  as: "exceptions"
+});
+db.TransportException.belongsTo(db.Route, {
+  foreignKey: "route_id",
+  as: "route"
+});
+
+db.Student.hasMany(db.TransportException, {
+  foreignKey: "student_id",
+  as: "transportExceptions"
+});
+db.TransportException.belongsTo(db.Student, {
+  foreignKey: "student_id",
+  as: "student"
+});
+
+db.User.hasMany(db.TransportException, {
+  foreignKey: "resolved_by",
+  as: "resolvedExceptions"
+});
+db.TransportException.belongsTo(db.User, {
+  foreignKey: "resolved_by",
+  as: "resolvedBy"
+});
+
+db.Route.hasOne(db.DriverLocation, {
+  foreignKey: "route_id",
+  as: "liveLocation"
+});
+db.DriverLocation.belongsTo(db.Route, { foreignKey: "route_id", as: "route" });
+
+db.User.hasMany(db.DriverLocation, {
+  foreignKey: "driver_id",
+  as: "liveLocations"
+});
+db.DriverLocation.belongsTo(db.User, { foreignKey: "driver_id", as: "driver" });
+
+module.exports = db;
---
+++ config/socketConfig.js
@@ -0,0 +1,343 @@
+const redisClient = require("./redisConfig.js");
+const { getIO } = require("./socketSetup.js");
+const db = require("./db.js");
+const jwt = require("jsonwebtoken");
+
+var emitToSockets = async (userId, eventName, data) => {
+  // console.log("userId, eventName, data", userId, eventName, data);
+  const socketIds = await redisClient.lrange(`user:${userId}:sockets`, 0, -1);
+  console.log("socketIds", socketIds, userId, eventName);
+  try {
+    socketIds.forEach((socketId) => {
+      const socket = getIO().sockets.sockets.get(socketId);
+      if (socket) {
+        console.log("Emit Sent", userId, eventName);
+        socket.emit(eventName, data);
+      }
+    });
+  } catch (error) {
+    console.log("error in emitToSocket", error);
+  }
+};
+
+const getSocketCount = async () => {
+  try {
+    const userKeys = await redisClient.keys("user:*:sockets");
+    let totalSocketCount = 0;
+
+    for (const key of userKeys) {
+      const socketIds = await redisClient.lrange(key, 0, -1);
+      totalSocketCount += socketIds.length;
+    }
+
+    return totalSocketCount;
+  } catch (error) {
+    console.error("Error getting socket count:", error);
+    return 0;
+  }
+};
+
+const emitToSocketById = async (socketId, eventName, data) => {
+  // console.log("socketId, eventName, data", socketId, eventName, data);
+  try {
+    const socket = getIO().sockets.sockets.get(socketId);
+    if (socket) {
+      // console.log("Emit Sent to socketId:", socketId);
+      socket.emit(eventName, data);
+    } else {
+      console.log("Socket not found for socketId:", socketId);
+    }
+  } catch (error) {
+    console.log("Error in emitToSocketById", error);
+  }
+};
+
+const getUniqueJoinUserIdsByChatId = async (chat_id, user_id) => {
+  try {
+    // Fetch sockets and chat data concurrently
+    const [sockets, chat] = await Promise.all([
+      getIO().in(parseInt(chat_id)).fetchSockets(),
+      db.Chat.findByPk(chat_id)
+    ]);
+
+    if (!chat) {
+      throw new Error("Chat not found");
+    }
+
+    // Determine sender and receiver
+    const sender = user_id;
+    const receiver = chat.chat_by === user_id ? chat.chat_to : chat.chat_by;
+
+    // Fetch tokens for sender and receiver concurrently
+    const [senderTokens, receiverTokens] = await Promise.all([
+      db.Token.findAll({
+        where: { user_id: sender },
+        attributes: [
+          "device_token",
+          "user_id",
+          [db.Sequelize.literal("true"), "is_sender"]
+        ],
+        raw: true // Use raw: true to avoid mapping .get({ plain: true })
+      }),
+      db.Token.findAll({
+        where: { user_id: receiver },
+        attributes: [
+          "device_token",
+          "user_id",
+          [db.Sequelize.literal("false"), "is_sender"]
+        ],
+        raw: true
+      })
+    ]);
+
+    // Helper function to enrich tokens with socket data
+    const enrichTokens = async (tokens, user_id) => {
+      const redisKey = `user:${user_id}:sockets`;
+      const redisSocketIds = await redisClient.lrange(redisKey, 0, -1);
+
+      return tokens.map((token) => {
+        // Check if token matches any socket in the room
+        const socket = sockets.find(
+          (s) =>
+            s.user_id === token.user_id && s.device_token === token.device_token
+        );
+
+        if (socket) {
+          return {
+            ...token,
+            socket_id: socket.id,
+            is_join_room: true
+          };
+        }
+
+        // Fallback to Redis if no socket is found in the room
+        const redisSocket = redisSocketIds.find((s) => {
+          // const parsed = JSON.parse(s); // Assuming Redis stores JSON strings
+          const parsed = s; // Assuming Redis stores JSON strings
+          let socket = getIO().sockets.sockets.get(parsed);
+          // console.log("socket", socket)
+          if (
+            socket &&
+            socket.user_id === token.user_id &&
+            socket.device_token === token.device_token
+          ) {
+            return socket;
+          } else {
+            return null;
+          }
+        });
+        return {
+          ...token,
+          socket_id: redisSocket ? redisSocket : null,
+          is_join_room: false
+        };
+      });
+    };
+
+    // Enrich sender and receiver tokens
+    const [enrichedSenderTokens, enrichedReceiverTokens] = await Promise.all([
+      enrichTokens(senderTokens, sender),
+      enrichTokens(receiverTokens, receiver)
+    ]);
+
+    // Combine tokens and compute unique user IDs
+    const allSocketsWithTokens = [
+      ...enrichedSenderTokens,
+      ...enrichedReceiverTokens
+    ];
+    const uniqueJoinUserIds = [
+      ...new Set(sockets.map((token) => token.user_id))
+    ];
+
+    // Log for debugging (optional, can be removed in production)
+    // console.log('senderTokens:', enrichedSenderTokens);
+    // console.log('receiverTokens:', enrichedReceiverTokens);
+    // console.log('uniqueJoinUserIds:', uniqueJoinUserIds);
+
+    return {
+      uniqueJoinUserIds,
+      enrichedSenderTokens,
+      enrichedReceiverTokens,
+      allSocketsWithTokens
+    };
+  } catch (error) {
+    console.error("Error fetching socket data:", error);
+    return {
+      uniqueJoinUserIds: [],
+      enrichedSenderTokens: [],
+      enrichedReceiverTokens: [],
+      allSocketsWithTokens: []
+    };
+  }
+};
+
+function socketConfig(io) {
+  io.on("connection", async (socket) => {
+    console.log("connection", socket.id);
+    socket.on("socket_register", async function (data) {
+      console.log("socket_register ->", data.user_id, socket.id, data.token_id);
+      let user_id = data.user_id;
+      if (!user_id) {
+        return;
+      }
+      socket.user_id = user_id;
+
+      if (data.token_id) {
+        const token_id = data.token_id;
+
+        const token = await db.Token.findOne({ where: { id: token_id } });
+        if (token) {
+          socket.device_token = token.device_token;
+          socket.device_id = token.device_id;
+        }
+      }
+
+      const redisKey = `user:${user_id}:sockets`;
+
+      try {
+        // Start a transaction
+        const multi = redisClient.multi();
+
+        // Check existing socket IDs
+        multi.lrange(redisKey, 0, -1);
+        const existingSocketIds = await multi.exec();
+
+        // Update user status
+        await db.User.update({ status: "Online" }, { where: { id: user_id } });
+
+        // Check if the socket ID is already registered
+        if (existingSocketIds[0].includes(socket.id)) {
+          console.log(
+            `Socket ID ${socket.id} already registered for user ${user_id}`
+          );
+          return; // Skip the registration if the socket ID is already present
+        }
+
+        // If not already registered, add the socket ID to the list
+        await redisClient.lpush(redisKey, socket.id);
+        io.emit("active_status", { user_id: user_id, status: "Online" });
+
+        // Retrieve and log all socket IDs for the user
+        const socketIds = await redisClient.lrange(redisKey, 0, -1);
+        console.log("socketIds in socket_register-->", user_id, socketIds);
+      } catch (error) {
+        console.error("Error in socket_register:", error);
+      }
+    });
+
+    socket.on("join_room", async function (data) {
+      console.log("join_room called", data);
+      var user_id = data.user_id;
+      var chat_id = parseInt(data.chat_id);
+      socket.user_id = user_id;
+      console.log("socket.join(chat_id)", chat_id);
+      socket.join(chat_id);
+      try {
+        await emitToSockets(user_id, "join_room", {
+          Message: "Successful",
+          info: data
+        });
+      } catch (error) {
+        console.log("join_room emit not send");
+      }
+    });
+
+    socket.on("left_room", async function (data) {
+      console.log("left_room called", data);
+      var chat_id = parseInt(data.chat_id);
+      socket.leave(chat_id);
+      try {
+        await emitToSockets(data.user_id, "left_room", {
+          Message: "Successful",
+          info: data
+        });
+      } catch (error) {
+        console.log("left_room emit not send");
+      }
+    });
+
+    socket.on("disconnect", async (sockets) => {
+      console.log("disconnectedSocketId", socket.id, socket.user_id);
+      try {
+        await redisClient.lrem(`user:${socket.user_id}:sockets`, 0, socket.id);
+      } catch (e) {
+        console.log("id is not found ->>", socket.id);
+      }
+    });
+
+    socket.on("join_group", async function (data) {
+      console.log("join group called", data);
+      let user_id = data.user_id;
+      let chatroomId = parseInt(data.school_id);
+      socket.user_id = user_id;
+      socket.join(`lesson_chat_${chatroomId}`);
+      try {
+        await commonService.emitToSockets(data.user_id, "join_group", {
+          Message: "Successful",
+          info: data
+        });
+        console.log(`join_group emit send to user ${data.user_id}`);
+      } catch (error) {
+        console.log("join_group emit not send");
+      }
+    });
+
+    socket.on("left_group", async function (data) {
+      console.log("left_group", data);
+      let chatroomId = parseInt(data.school_id);
+      socket.leave(`lesson_chat_${chatroomId}`);
+      try {
+        await commonService.emitToSockets(data.user_id, "left_group", {
+          Message: "group left Successful",
+          info: data
+        });
+        console.log(`left_group emit send to user ${data.user_id}`);
+      } catch (error) {
+        console.log("left_group emit not send");
+      }
+    });
+
+    global.socket_id = socket.id;
+  });
+
+  const SOCKET_TIMEOUT = 30000; // Timeout value in milliseconds (adjust as needed)
+
+  // Function to periodically check and disconnect sockets whose IDs have been removed from Redis
+  setInterval(async () => {
+    try {
+      // Get all user IDs stored in Redis
+      const userIds = await redisClient.keys("user:*:sockets");
+      // console.log("userIds=====>>>", userIds);
+      // Iterate through each user ID
+      for (const userId of userIds) {
+        // console.log('userId===>>>', userId);
+        // Get all socket IDs associated with the current user from Redis
+        const socketIds = await redisClient.lrange(userId, 0, -1);
+        // console.log("socketIdss in disconnect",socketIds)
+        // Iterate through each socket ID
+        for (const socketId of socketIds) {
+          // Check if the socket corresponding to the socket ID is disconnected
+          const socket = io.sockets.sockets.get(socketId);
+
+          if (!socket || !socket.connected) {
+            // Socket is disconnected, disconnect it
+            // console.log("Disconnecting socket:", socketId);
+            await redisClient.lrem(userId, 0, socketId);
+            // io.emit("active_count", { active_count: await getSocketCount() })
+            io.emit("active_status", { user_id: userId, status: "Offline" });
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error checking and disconnecting sockets:", error);
+    }
+  }, SOCKET_TIMEOUT); // Run the check every SOCKET_TIMEOUT milliseconds
+}
+
+module.exports = {
+  socketConfig,
+  emitToSockets,
+  getSocketCount,
+  getUniqueJoinUserIdsByChatId,
+  emitToSocketById
+};
